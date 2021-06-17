9dd7226c259adb8a047639d6bc4218794e7f3ab4
Whiting <whitinghtennis@gmail.com>
2021-06-17 12:57:51 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const mainMain = document.getElementById('main');
+mainMain.remove();
+
+const newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.setAttribute('id', "victory");
+
+newHeader.innerHTML = "Whiting is the champion";
